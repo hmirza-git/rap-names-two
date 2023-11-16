f5479e7b318ca40eb81f3996cd0a1feaec4ff13c
Mirza Hassan <hassanmirza621@gmail.com>
2023-11-17 01:58:41 +0500

    installed cors

--- public/main.js
+++ public/main.js
@@ -1,3 +1,11 @@
-document.querySelector("h2").innerHTML = "hello";
 alert("hi");
-console.log("testing");
+
+async function rapperAPI() {
+  const res = await fetch(`https://rap-names-two.vercel.app/api/21savage`);
+  const data = await res.json();
+  console.log(data);
+}
+
+rapperAPI();
+
+document.querySelector("h2").innerHTML = "hello";
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 8000;
+app.use(cors());
 app.use(express.static("public"));
 const rappers = {
   "21savage": {
