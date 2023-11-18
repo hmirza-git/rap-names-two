8237c26f1540b0c91e0805ec51cd74145259a644
Mirza Hassan <hassanmirza621@gmail.com>
2023-11-19 00:49:03 +0500

    e

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+alert("hi");
+const inputEl = document.querySelector("input");
+async function rapperAPI(input) {
+  const res = await fetch(`https://rap-names-two.vercel.app/api/${input}`);
+  const data = await res.json();
+  console.log(data);
+  document.querySelector("h2").innerHTML = data.realName;
+}
+
+document.querySelector("button").addEventListener("click", () => {
+  rapperAPI(inputEl.value);
+});
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
